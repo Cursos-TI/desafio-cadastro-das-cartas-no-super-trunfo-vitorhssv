321d6db1286a75fc8bb0442b13b4be1754267b5e
Vitor <121695616+vitorhssv@users.noreply.github.com>
2025-03-04 18:44:53 -0300

    Nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 int main() {
     // Variáveis
     int populacao, pontos_turisticos;
-    float area, pib;
+    float area, pib, densidade_populacional, pib_per_capita;
 
     // Cadastro da carta 1
     printf("CADASTRO DA CARTA 1");
@@ -22,12 +22,17 @@ int main() {
     printf("\nDigite a quantidade de pontos turísticos da carta: ");
     scanf("%d", &pontos_turisticos);
 
+    densidade_populacional = populacao / area;
+    pib_per_capita = pib / populacao;
+
     // Exibição da carta 1
     printf("\nCARTA 1\n");
     printf("População: %d\n", populacao);
     printf("Área: %.2f\n", area);
     printf("PIB: %.2f\n", pib);
     printf("Pontos turísticos: %d\n", pontos_turisticos);
+    printf("Densidade populacional: %.2f\n", densidade_populacional);
+    printf("PIB per capita: %.2f\n", pib_per_capita);
 
     // Cadastro da carta 2
     printf("\nCADASTRO DA CARTA 2");
@@ -43,12 +48,17 @@ int main() {
     printf("\nDigite a quantidade de pontos turísticos da carta: ");
     scanf("%d", &pontos_turisticos);
 
+    densidade_populacional = populacao / area;
+    pib_per_capita = pib / populacao;
+
     // Exibição da carta 2
     printf("\nCARTA 2\n");
     printf("População: %d\n", populacao);
     printf("Área: %.2f\n", area);
     printf("PIB: %.2f\n", pib);
-    printf("Pontos turísticos: %d", pontos_turisticos);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+    printf("Densidade populacional: %.2f\n", densidade_populacional);
+    printf("PIB per capita: %.2f\n", pib_per_capita);
 
     return 0;
 }
